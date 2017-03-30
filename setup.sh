aa131c9abaf23480c87baccbed348cbca4da8454
Sindre Gulseth <sindre.gulseth@vg.no>
2017-03-30 10:07:31 +0200

    Added tmuxinator

--- setup.sh
+++ setup.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-rm -f ~/.zshrc ~/.tmux.conf ~/.vimrc
+if [ -f ~/.zshrc ]; then
+    echo "zshrc file exists. Backing up."
+    mv ~/.zshrc ~/.zshrc-old
+fi
+
+if [ -f ~/.tmux.conf ]; then
+    echo "tmux.conf file exists. Backing up."
+    mv ~/.tmux.conf ~/.tmux.conf-old
+fi
+
+if [ -f ~/.vimrc ]; then
+    echo "vimrc file exists. Backing up."
+    mv ~/.vimrc ~/.vimrc-old
+fi
+
+if [ -d ~/.tmuxinator ]; then
+    echo "tmuxinator dir exists. Backing up."
+    mv ~/.tmuxinator ~/.tmuxinator-old
+fi
 
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmuxinator ~/.tmuxinator
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 
 cd ~/.dotfiles/
-git submodule init 
+git submodule init
 git submodule update
 vim +PluginInstall all
